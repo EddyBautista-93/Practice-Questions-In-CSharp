e6cad71e5fbc56a4c492f231b922062ade6df1cf
Eddy Bautista <bautista.j.eddy@gmail.com>
2019-12-31 07:51:25 -0600

    Initial check-in of module PracticeQuestionsInCSharp

---
+++ PracticeQuestionsInCSharp/Program.cs
@@ -0,0 +1,22 @@
+﻿using System;
+
+namespace PracticeQuestionsInCSharp
+{
+    class MainClass
+    {
+        // Even or odd with switch statement
+        public static string evenOrOdd(int number)
+        {
+            return (number % 2 == 0) ? "Even" : "Odd";
+        }
+
+        public static void Main(string[] args)
+        {
+            Console.WriteLine(evenOrOdd(1));
+            Console.WriteLine(evenOrOdd(2));
+            Console.WriteLine(evenOrOdd(3));
+            Console.WriteLine(evenOrOdd(4));
+            Console.WriteLine(evenOrOdd(5));
+        }
+    }
+}
