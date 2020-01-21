771598a0c3009aac01c359291786f4a01cb1d988
Eddy Bautista <bautista.j.eddy@gmail.com>
2020-01-21 05:04:19 -0600

    added peopleinline question from codewars

--- PracticeQuestionsInCSharp/Program.cs
+++ PracticeQuestionsInCSharp/Program.cs
@@ -193,6 +193,34 @@ namespace PracticeQuestionsInCSharp
         //FIRST go in expression bodied members in c#
         public static string Speak(string name) => $"One for {name}, one for me.";
 
+        public static string tickets(int[] peopleInLine)
+        {
+            int d25 = 0;
+            int d50 = 0;
+            for (int i = 0; i < peopleInLine.Length; i++)
+            {
+                if (peopleInLine[i] == 25) d25++;
+                if (peopleInLine[i] == 50)
+                {
+                    d25--;
+                    d50++;
+                }
+                if (peopleInLine[i] == 100)
+                {
+                    if (d50 > 0)
+                    {
+                        d50--;
+                        d25--;
+                    }
+                    else
+                    {
+                        d25 -= 3;
+                    }
+                }
+                if (d25 < 0) return "NO";
+            }
+            return "YES";
+        }
 
 
 
