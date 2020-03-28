7da8c86591fd65a3e17cbdfcd214946b7485448b
Eddy Bautista <bautista.j.eddy@gmail.com>
2020-03-27 21:55:04 -0500

    added solution

--- PracticeQuestionsInCSharp/Program.cs
+++ PracticeQuestionsInCSharp/Program.cs
@@ -262,11 +262,46 @@ namespace PracticeQuestionsInCSharp
 
         }
 
-        public static int diagonalDifference(List<List<int>> arr)
+        public static int difference(int[,] arr,
+                                 int n)
         {
 
+            // Initialize sums of diagonals 
+            int d1 = 0, d2 = 0;
+
+            for (int i = 0; i < n; i++)
+            {
+                for (int j = 0; j < n; j++)
+                {
+
+                    // finding sum of primary diagonal 
+                    if (i == j)
+                        d1 += arr[i, j];
+
+                    // finding sum of secondary diagonal 
+                    if (i == n - j - 1)
+                        d2 += arr[i, j];
+                }
+            }
+
+            // Absolute difference of the 
+            // sums across the diagonals 
+            return Math.Abs(d1 - d2);
         }
 
+        // Driver Code 
+        public static void Main()
+        {
+            int n = 3;
+
+            int[,] arr ={{11, 2, 4},
+                     {4 , 5, 6},
+                     {10, 8, -12}};
+
+            Console.Write(difference(arr, n));
+
+        }
+    }
 
 
 
@@ -274,7 +309,8 @@ namespace PracticeQuestionsInCSharp
 
 
 
-        public static void Main(string[] args)
+
+    public static void Main(string[] args)
         {
             //Console.WriteLine(evenOrOdd(1));
             //Console.WriteLine(evenOrOdd(2));
